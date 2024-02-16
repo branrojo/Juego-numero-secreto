4caf7311026d98e55de53ab1ce01c72bf277091a
branrojo <120753875+branrojo@users.noreply.github.com>
2024-02-16 12:39:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ function asignarTextoElemento(elemento, texto) {
     elementoHTML.innerHTML = texto;
 }
 
-let numeroSecreto = 0;
-let intentos = 0;
-let listaNumerosSorteados = [];
-let numeroMaximo = 10;
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-}
-
 function verificarIntento() {
     let numeroDeUsuario = document.getElementById('valorUsuario').value.trim();
 
@@ -45,40 +35,6 @@ function verificarIntento() {
     limpiarCaja();
 }
 
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
-}
-
-function generarNumeroSecreto() {
-    let numeroGenerado =  Math.floor(Math.random() * numeroMaximo) + 1;
-
-    if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
-    } else {
-        if (listaNumerosSorteados.includes(numeroGenerado)) {
-            return generarNumeroSecreto();
-        } else {
-            listaNumerosSorteados.push(numeroGenerado);
-            return numeroGenerado;
-        }
-    }
-}
-
-function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 0; // Reiniciar el número de intentos
-}
-
-function reiniciarJuego() {
-    limpiarCaja();
-    condicionesIniciales();
-    document.getElementById('reiniciar').setAttribute('disabled','true');
-}
-
-condicionesIniciales();
-}
 
 function limpiarCaja() {
     document.querySelector('#valorUsuario').value = '';
